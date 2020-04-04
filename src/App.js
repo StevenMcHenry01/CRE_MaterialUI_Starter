eda201dac5d4c9df5faa6cae79d02c69d1ed69f2
Steven McHenry <stevenmchenry01@gmail.com>
2020-04-04 16:23:31 -0700

    default

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import TopLayout from './components/Layout'
+import Programmer from './components/Shape.so/Programmer'
+import { Container, Box } from '@material-ui/core'
 
 function App() {
   return (
     <TopLayout>
-      <div>Hello</div>
+      <Container>
+        <Box>
+          <div>Hello</div>
+          <Programmer />
+        </Box>
+      </Container>
     </TopLayout>
   )
 }
