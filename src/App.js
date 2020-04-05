3d9c6163940e7970039ce90e1b7fdd2554e67901
Steven McHenry <stevenmchenry01@gmail.com>
2020-04-04 17:18:02 -0700

    default

--- src/App.js
+++ src/App.js
@@ -12,11 +12,37 @@ function App() {
     padding: 2rem 0;
   `
 
+  const FlexContainer = styled.div`
+    display: flex;
+    background: ${theme.palette.grey[200]};
+    flex-wrap: wrap;
+  `
+
+  const BoxFlex = styled.div`
+    /* margin: 0 1rem; */
+    height: 150px;
+    flex-basis: 300px;
+    flex-grow: 1;
+    &.one {
+      background: ${theme.palette.warning.main};
+    }
+    &.two {
+      background: ${theme.palette.primary.main};
+    }
+    &.three {
+      background: green;
+    }
+  `
+
   // ~ COMPONENT
   return (
     <TopLayout>
       <MainContentContainerStyled maxWidth='sm'>
-        <div>Hello</div>
+        <FlexContainer>
+          <BoxFlex className='one'></BoxFlex>
+          <BoxFlex className='two'></BoxFlex>
+          <BoxFlex className='three'></BoxFlex>
+        </FlexContainer>
       </MainContentContainerStyled>
     </TopLayout>
   )
